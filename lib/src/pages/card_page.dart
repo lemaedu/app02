bb27218dbc2183eb79e471be798b7d6232451128
lemaedu <lemaedu@gmail.com>
2021-08-04 08:45:06 -0500

    71. Card Widget

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,12 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
-        children: <Widget>[_cardTipo1()],
+        children: <Widget>[
+          _cardTipo1(),
+          //Añade espacion entre los dos elementos
+          SizedBox(height: 30.0),
+          _cardTipo2()
+        ],
       ),
     );
   }
@@ -36,6 +41,36 @@ class CardPage extends StatelessWidget {
                 onPressed: () {},
               ),
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.add_box, color: Colors.blue),
+            title: Text('Titulo de la Targeta'),
+            subtitle: Text("Aqui va el subtitulo de la tarjeta"),
+          ),
+          //witgest para loadings para imagenes
+          FadeInImage(
+            image: NetworkImage(
+                'https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 300),
+            height: 250.00,
+            fit: BoxFit.cover,
+          ),
+
+          //Cada vez que se pone recursos estaticos es recomendable hacer un fullrestart
+
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Texto de Container de flutter'),
           )
         ],
       ),
