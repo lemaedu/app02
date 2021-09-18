79d752e81588efb1ccc2297a863f8a77b5d9f27f
lemaedu <lemaedu@gmail.com>
2021-09-18 16:14:35 -0500

    75 Witget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,14 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar Alerta'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue[800], shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.west_outlined),
         onPressed: () {
@@ -16,5 +24,41 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        //Permite cerrar la ventana al hacer clic afuera del dialog
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              title: Text('Titulo'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Contenido de la ventana'),
+                  FlutterLogo(
+                    size: 75.0,
+                  )
+                ],
+              ),
+              actions: <Widget>[
+                ElevatedButton(
+                  child: Text('Cancelar'),
+                  onPressed: () => Navigator.of(context).pop(),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.red, shape: StadiumBorder()),
+                ),
+                ElevatedButton(
+                  child: Text('Ok'),
+                  onPressed: () => {Navigator.of(context).pop()},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue[800], shape: StadiumBorder()),
+                ),
+              ]);
+        });
   }
 }
