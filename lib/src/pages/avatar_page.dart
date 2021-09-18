ba0dde7d60156c223400a7231e805f497769c166
lemaedu <lemaedu@gmail.com>
2021-09-18 17:34:36 -0500

    76. Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/16251556?v=4"),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.white70,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              "https://avatars.githubusercontent.com/u/16251556?v=4"),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(microseconds: 200),
+        ),
       ),
     );
   }
