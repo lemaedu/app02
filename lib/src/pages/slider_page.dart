64d758a284807818c8077a25f52d2a719ad8c94f
lemaedu <lemaedu@gmail.com>
2021-10-09 12:38:33 -0500

    83. Slider

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+        activeColor: Colors.indigoAccent,
+        label: 'Tamño de Imagen',
+        //divisions: 20,
+        value: _valorSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: (valor) {
+          setState(() {
+            _valorSlider = valor;
+          });
+        });
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image:
+          NetworkImage('https://avatars.githubusercontent.com/u/16251556?v=4'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:app02/src/pages/avatar_page.dart';
 import 'package:app02/src/pages/home_page.dart';
 import 'package:app02/src/pages/card_page.dart';
 import 'package:app02/src/pages/animated_container.dart';
+import 'package:app02/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -14,5 +15,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
   };
 }
